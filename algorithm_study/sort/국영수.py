d395ef62acc606e0035a61db8d77f79c6deaf14b
baejinsoo <jinsoo941010@naver.com>
2020-10-17 01:29:40 +0900

    modify readme

--- algorithm_study/sort/국영수.py
+++ algorithm_study/sort/국영수.py
@@ -1,1 +1,12 @@
 # 국영수 BOJ 10825
+
+n = int(input())
+students = []
+
+for _ in range(n):
+    students.append(input().split())
+
+students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in students:
+    print(student[0])
